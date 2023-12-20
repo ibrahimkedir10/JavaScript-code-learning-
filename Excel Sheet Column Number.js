76aead50be148255aade121efce2bb4af08aa559
ibrahim.kedir <ibrahim.kedir@outlook.com>
2023-12-20 15:25:59 -0500

    Update Excel Sheet Column Number.js

--- Excel Sheet Column Number.js
+++ Excel Sheet Column Number.js
@@ -25,3 +25,43 @@ Example 3:
 Input: columnTitle = "ZY"
 Output: 701
 */
+
+/*
+// LETTERS  => NUMBERS:
+// corospong each letter with a number
+//- possible problems: when letters are large-> infinit (take too long to type each possible pair)
+var convertToTitle = function(columnNumber) {
+
+    
+    // create a varable to hold result Number : resultNum
+    let resultNum = 0;
+    // find out low many letters / columNumber.length  there are ex: _ or _ _ or _ _ _ ...
+    for(let i = 0; i < columnNumber.length ; i++){
+        // varable to convert and store letter to number : letterVal
+        const letterVal = (columnNumber.charCodeAt(i) - "A".charCodeAt(0) +1) ;
+        resultNum = resultNum * 26 + letterVal;
+      
+    }
+   
+    return resultNum
+
+
+};
+
+console.log(convertToTitle("AB"));
+*/
+
+
+//NUMBERS TO LETTERS 
+var convertToTitle = function(columnNumber) {
+    // create varable to hold output sting 
+    let results = "";
+    
+    // create a forloop that goes through the  number length 
+    for(let i = 0; i< columnNumber ; i++){
+        // varable to hold coverted value:
+        const convert = columnNumber.charCodeAT
+    }
+};
+
+    convertToTitle(10);
