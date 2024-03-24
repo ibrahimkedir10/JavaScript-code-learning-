b5efabbf8b2f5f5a30046461f924b9118fc92705
ibrahim.kedir <ibrahim.kedir@outlook.com>
2024-03-24 16:58:01 -0400

    testing code with jest

---
+++ jestTesting/main.js
@@ -0,0 +1,9 @@
+function fetchPromise(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+        }, 1000);
+    });
+}
+
+module.export = fetchPromise;
---
+++ jestTesting/test.js
@@ -0,0 +1,3 @@
+
+
+const fetchPromise = require(./main);
